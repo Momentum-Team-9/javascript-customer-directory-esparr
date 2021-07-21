892559a04d9f3e4ba2d45cebdbafeb9bbc1955c3
Em <amberem@gmail.com>
2021-07-21 08:32:55 -0400

    Some tweaks to the main.js to be a little more streamlined.

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-console.log(customers);
-
 // Calls the root div
 const outputDiv = document.getElementById("root");
 
+// Calls the function
 outputCustomerCards(customers);
 
+// Function for customer cards
 function outputCustomerCards(customers) {
   for (i = 0; i < customers.length; i++) {
     console.log(i);
@@ -19,11 +19,9 @@ function outputCustomerCards(customers) {
       customerCard.appendChild(customerPic);
 
       // Puts First and Last Name on customer card
-      const firstName = document.createElement("h2");
-      firstName.innerText = customers[i].name.first;
-      const lastName = customers[i].name.last;
-      firstName.innerText += " " + lastName;
-      customerCard.appendChild(firstName);
+      const Name = document.createElement("h2");
+      Name.innerText = customers[i].name.first + " " + customers[i].name.last;
+      customerCard.appendChild(Name);
 
       // Puts Email Address on customer card
       const email = document.createElement("h3");
@@ -34,15 +32,20 @@ function outputCustomerCards(customers) {
       // Puts Address on customer card
       const addressLine1 = document.createElement("p");
       addressLine1.classList.add("address");
-      addressLine1.innerText = customers[i].location.street.number;
-      addressLine1.innerText += " " + customers[i].location.street.name;
+      addressLine1.innerText =
+        customers[i].location.street.number +
+        " " +
+        customers[i].location.street.name;
       customerCard.appendChild(addressLine1);
 
       const addressLine2 = document.createElement("p");
       addressLine2.classList.add("address");
-      addressLine2.innerText = customers[i].location.city + ",";
-      addressLine2.innerText += " " + nameToAbbr(customers[i].location.state);
-      addressLine2.innerText += " " + customers[i].location.postcode;
+      addressLine2.innerText =
+        customers[i].location.city +
+        ", " +
+        nameToAbbr(customers[i].location.state) +
+        " " +
+        customers[i].location.postcode;
       customerCard.appendChild(addressLine2);
 
       // Puts Date of Birth on customer card
